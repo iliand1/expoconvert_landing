adc4376641632455ade9aa6992254332aa16752a
Denis Burkatsky <bluemahogony@gmail.com>
2025-01-22 13:49:09 +0800

    button renamed

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -49,7 +49,7 @@ const Navigation: React.FC = () => {
             className="text-sm ml-4 flex items-center bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-600 hover:from-blue-600 hover:via-indigo-600 hover:to-purple-700 transition-all duration-200 text-white font-semibold" 
             onClick={() => window.open('https://calendly.com/denis_burkatksiy/30min', '_blank')}
           >
-            Book Discovery Session <Rocket className="ml-2 h-4 w-4" />
+            Map Out Your Next Expo <Rocket className="ml-2 h-4 w-4" />
           </Button>
         </div>
       </div>
